89878f21a6e61a7be45a7c0502bc59efb6b3b0a8
Pavel Stepanov <impetus@yandex.ru>
2024-02-12 10:29:07 +0300

    Add Toast + Snackbar

--- app/src/main/java/com/example/myapplication/MainActivity2.java
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -3,6 +3,9 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +13,7 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        //Toast.makeText(this,"Сообщение",Toast.LENGTH_LONG).show();
+        Snackbar.make(findViewById(R.id.root),"Hello", Snackbar.LENGTH_SHORT).show();
     }
 }
